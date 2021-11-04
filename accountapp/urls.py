1b527589585f1e88512ed6b1fe1ca61bfc50bf2e
dassolkim <kimds0926@gmail.com>
2021-11-04 21:24:19 +0900

    add login/out function

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from .views import hello_world, AccountCreateView
@@ -9,5 +10,8 @@ urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
     path('hello_list/', hello_object_list, name='hello_object_list'),
     # 함수형 view와의 차이점: 이름을 바로 쓸 수 없음
-    path('create/', AccountCreateView.as_view(), name='create')
+    path('create/', AccountCreateView.as_view(), name='create'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
+
 ]
