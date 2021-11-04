5bcba7be9e09980b39636d841986710f536e8fd3
dassolkim <kimds0926@gmail.com>
2021-11-04 15:08:12 +0900

    test POST api

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp'
+    'accountapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
