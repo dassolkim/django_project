c83a120edccdafe6c545a26550e38a34e12e7b82
dassolkim <kimds0926@gmail.com>
2021-11-04 22:20:56 +0900

    add delete view

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from .views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from .views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 from .views import hello_object_list
 
 app_name = "accountapp"
@@ -14,7 +14,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', AccountUpdateView.as_view(), name='update')
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete')
+
 
 
 
