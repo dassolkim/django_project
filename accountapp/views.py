d002c36f6bcec5a2a3b30bc8a03f37d2675803df
dassolkim <kimds0926@gmail.com>
2021-11-04 15:32:46 +0900

    add GET  print_object_list

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from .models import HelloWorld
 # Create your views here.
 
@@ -9,8 +11,11 @@ def hello_world(request):
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_output': new_hello_world})
+
+        # return render(request, 'accountapp/hello_world.html',
+        #               context={'hello_world_output': new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
+        hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET Method!'})
+                      context={'hello_world_list': hello_world_list})
